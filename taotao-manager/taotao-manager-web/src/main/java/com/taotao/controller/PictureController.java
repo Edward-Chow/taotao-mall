b65c878e8a798c9bb9a0ba479dabe497083c5f75
Gershon-Chow <superm0nk3y@outlook.com>
2018-08-18 21:34:06 +0800

    Third day of development of taotao-mall, finished the function of picture-uploading, using fastDFS as picture server.

---
+++ taotao-manager/taotao-manager-web/src/main/java/com/taotao/controller/PictureController.java
@@ -0,0 +1,22 @@
+package com.taotao.controller;
+
+import com.taotao.common.pojo.PictureResult;
+import com.taotao.service.PictureService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
+public class PictureController {
+
+    @Autowired
+    private PictureService pictureService;
+
+    @RequestMapping("pic/upload")
+    @ResponseBody
+    public PictureResult uploadFile(MultipartFile uploadFile) {
+        return pictureService.uploadPic(uploadFile);
+    }
+}
